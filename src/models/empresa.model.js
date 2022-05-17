f6491fbeb48df7052dcdbde34e88d1c1f81e0342
Andy Cuyuch <acuyuch-2017403@kinal.edu.gt>
2022-05-16 19:57:02 -0600

    Se termino de crear el modelo de empresa

---
+++ src/models/empresa.model.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const moongose = require('mongoose');
+
+const empresaSchema = moongose.Schema({
+    name: String, 
+    typeOfCompany: String, 
+    municipality: String,  
+    password: String
+});
+
+module.exports = moongose.model('Empresa', empresaSchema);
+
+
+
